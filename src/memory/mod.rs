1e101deb3dcdd99781939f25a3dc2bd947d1df34
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2015-09-21 15:24:59 +0200

    separate memory bank structure for easier IO/other memory access distinguishment

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,17 +1,126 @@
 #![allow(dead_code)]
 use utils;
+use std::ops::Index;
+
+enum MemType
+{
+    RAM,
+    BASIC,
+    CHARGEN,
+    IO,
+    KERNAL,
+}
+
+// specific memory bank - RAM, ROM, IO
+struct MemBank
+{
+    bank_type: MemType,
+    read_only: bool,
+    offset: u16,
+    data: Vec<u8>,
+}
+
+impl MemBank
+{
+    pub fn new(mem_type: MemType) -> MemBank
+    {
+        let mut mem_bank = MemBank
+        {
+            bank_type: mem_type,
+            read_only: true,
+            offset: 0x0000,
+            data: Vec::<u8>::new(),
+        };
+
+        match mem_bank.bank_type
+        {
+            MemType::BASIC   => {
+                mem_bank.data = utils::open_file("rom/basic.rom", 0);
+                mem_bank.offset = 0xA000;
+            },
+            MemType::CHARGEN => {
+                mem_bank.data = utils::open_file("rom/chargen.rom", 0);
+                mem_bank.offset = 0xD000;
+            },
+            MemType::KERNAL  => {
+                mem_bank.data = utils::open_file("rom/kernal.rom", 0);
+                mem_bank.offset = 0xE000;
+            },
+            MemType::RAM => {
+                mem_bank.data = Vec::<u8>::with_capacity(0x10000);
+                for _ in 0..0x10000
+                {
+                    mem_bank.data.push(0);
+                }
+                
+                mem_bank.read_only = false;
+            }
+            MemType::IO => {
+                mem_bank.data = Vec::<u8>::with_capacity(0x1000);
+                for _ in 0..0x1000
+                {
+                    mem_bank.data.push(0);
+                }
+
+                mem_bank.offset = 0xD000;
+                mem_bank.read_only = false;
+            }
+        }
+        
+        mem_bank
+    }
+
+    fn write(&mut self, addr: u16, val: u8)
+    {
+        match self.bank_type
+        {
+            MemType::RAM => self.data[(addr - self.offset) as usize] = val,
+            MemType::IO => {
+                // TODO: IO access has specific behavior depending on address
+                self.data[(addr - self.offset) as usize] = val;
+            },
+            _ => panic!("Can't write to ROM!")
+        }
+    }
+
+    fn read(&self, addr: u16) -> u8
+    {
+        match self.bank_type
+        {
+            MemType::IO => {
+                // TODO: IO access has specific behavior depending on address
+                self.data[(addr - self.offset) as usize]
+            },
+            _ => self.data[(addr - self.offset) as usize]
+        }
+    }    
+}
+
+impl Index<u16> for MemBank
+{
+    type Output = u8;
+
+    fn index<'a>(&'a self, _index: u16) -> &'a u8
+    {
+        &self.data[_index as usize]
+    }
+}
+
+
+// collective memory storage with all the banks and bank switching support
 pub struct Memory
 {
-    ram: [u8;0x10000],
-    basic: [u8;0x2000],
-    chargen: [u8;0x1000],
-    io: [u8;0x1000],
-    kernal: [u8;0x2000],
+    ram:     MemBank,
+    basic:   MemBank,
+    chargen: MemBank,
+    io:      MemBank,
+    kernal:  MemBank,
+
     // bank switching flags
-    basic_on: bool,
+    basic_on:   bool,
     chargen_on: bool,
-    io_on: bool,
-    kernal_on: bool,
+    io_on:      bool,
+    kernal_on:  bool,
     cart_lo_on: bool, // cart flag - unused for now
     cart_hi_on: bool  // cart flag - unused for now
 }
@@ -20,61 +129,35 @@ impl Memory
 {
     pub fn new() -> Memory
     {
-        let mut memory = Memory
+        Memory
         {
-            ram: [0;0x10000],   // 64k
-            basic: [0;0x2000],  // 8k
-            chargen: [0;0x1000], // 4k
-            io: [0;0x1000],      // 4k dedicated IO memory for VIC, SID, CIA registers [0xD000 - 0xDFFF]
-            kernal: [0;0x2000],  // 8k
-            basic_on: false,
+            ram:     MemBank::new(MemType::RAM),     // 64k
+            basic:   MemBank::new(MemType::BASIC),   // 8k
+            chargen: MemBank::new(MemType::CHARGEN), // 4k
+            io:      MemBank::new(MemType::IO),      // 4k (VIC, SID, CIA, Color RAM)
+            kernal:  MemBank::new(MemType::KERNAL),  // 8k
+            basic_on:   false,
             chargen_on: false,
-            io_on: false,
-            kernal_on: false,
+            io_on:      false,
+            kernal_on:  false,
             cart_lo_on: false, // unused for now
             cart_hi_on: false, // unused for now
-        };
-
-        // load basic
-        let basic = utils::open_file("rom/basic.rom", 0);
-        
-        for i in (0..0x2000)
-        {
-            memory.basic[i as usize] = basic[i as usize];
         }
-
-        // load chargen
-        let chargen = utils::open_file("rom/chargen.rom", 0);
-        
-        for i in (0..0x1000)
-        {
-            memory.chargen[i as usize] = chargen[i as usize];
-        }
-              
-        // load kernal
-        let kernal = utils::open_file("rom/kernal.rom", 0);
-        
-        for i in (0..0x2000)
-        {
-            memory.kernal[i as usize] = kernal[i as usize];
-        }
-        
-        memory
     }
     
-    // returns memory bank for current address + latch setting and an indicator whether it's read-only to detect ROM writes
-    pub fn get_bank(&mut self, addr: u16) -> (&mut [u8], bool, u16)
+    // returns memory bank for current latch setting and address
+    pub fn get_bank(&mut self, addr: u16) -> (&mut MemBank)
     {
         match addr
         {
-            0x0000...0x9FFF => (&mut self.ram[0..0x10000], false, 0),
-            0xA000...0xCFFF => if self.basic_on   { (&mut self.basic[0..0x2000], true, 0xA000) } else { (&mut self.ram[0..0x10000], false, 0) },
+            0x0000...0x9FFF => &mut self.ram,
+            0xA000...0xCFFF => if self.basic_on { &mut self.basic } else { &mut self.ram },
             0xD000...0xDFFF => {
-                if self.chargen_on { return (&mut self.chargen[0..0x1000], true, 0xD000); }
-                if self.io_on      { return (&mut self.io[0..0x1000], false, 0xD000); }
-                return (&mut self.ram[0..0x10000], false, 0);
+                if self.chargen_on { return &mut self.chargen }
+                if self.io_on      { return &mut self.io; }
+                return &mut self.ram;
             },
-            0xE000...0xFFFF => if self.kernal_on  { (&mut self.kernal[0..0x2000], true, 0xE000) } else { (&mut self.ram[0..0x10000], false, 0) },
+            0xE000...0xFFFF => if self.kernal_on  { &mut self.kernal } else { &mut self.ram },
             _ => panic!("Address out of memory range")
         }
     }
@@ -100,22 +183,16 @@ impl Memory
     // Write a byte to memory - returns whether RAM was written (true) or RAM under ROM (false)
     pub fn write_byte(&mut self, addr: u16, value: u8) -> bool
     {
-        // parentheses to avoid borrowing issues with changing the flags
-        {
-            let (_, read_only, offset) = self.get_bank(addr);
-            
             // RAM under ROM written? Return false to let us know about it
-            if read_only
+            if self.get_bank(addr).read_only
             {
-                self.ram[(addr - offset) as usize] = value;
+                self.ram.write(addr, value);
                 return false;
             }
             else
             {
-                let (bank, _, offset) = self.get_bank(addr);
-                bank[(addr - offset) as usize] = value;
+                self.get_bank(addr).write(addr, value);
             }
-        }
 
         // update the bank switching flags here, since they can only change on memory write
         // latch byte changed - update bank switching flags
@@ -127,16 +204,15 @@ impl Memory
     // Read a byte from memory
     pub fn read_byte(&mut self, addr: u16) -> u8
     {
-        let (bank, _, offset) = self.get_bank(addr);
-        bank[(addr - offset) as usize]
+        self.get_bank(addr).read(addr)
     }
 
     // Read a word from memory (stored in little endian)
     pub fn read_word_le(&mut self, addr: u16) -> u16
     {
-        let (bank, _, offset) = self.get_bank(addr);   
-        let value_be: u16 = ((bank[(addr - offset) as usize] as u16) << 8 & 0xFF00) |
-                            ((bank[(addr - offset + 0x0001) as usize] as u16) & 0x00FF);
+        let bank = self.get_bank(addr);
+        let value_be: u16 = ((bank.read(addr) as u16) << 8 & 0xFF00) |
+                            ((bank.read(addr + 0x0001) as u16) & 0x00FF);
 
         let value_le: u16 = ((value_be << 8) & 0xFF00) | ((value_be >> 8) & 0x00FF);
         value_le
@@ -145,9 +221,9 @@ impl Memory
     // Read a word from memory (stored in big endian: swap low<->high)
     pub fn read_word_be(&mut self, addr: u16) -> u16
     {
-        let (bank, _, offset) = self.get_bank(addr);
-        let value_le: u16 = ((bank[(addr - offset) as usize] as u16) << 8 & 0xFF00) |
-                            ((bank[(addr - offset + 0x0001) as usize] as u16) & 0x00FF);
+        let bank = self.get_bank(addr);
+        let value_le: u16 = ((bank.read(addr) as u16) << 8 & 0xFF00) |
+                            ((bank.read(addr + 0x0001) as u16) & 0x00FF);
         value_le
     }
 
