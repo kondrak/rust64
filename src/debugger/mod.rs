bd864ee7a92b8cd607de2bb4017898edc5d3a094
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2015-12-30 22:57:32 +0100

    [debug] added more diagnostic info

--- src/debugger/mod.rs
+++ src/debugger/mod.rs
@@ -15,7 +15,7 @@ pub struct Debugger
     debug_window: minifb::Window,
     font: font::SysFont,
     window_buffer: Vec<u32>,
-    mempage_offset: u16,
+    mempage_offset: u32,
 }
 
 impl Debugger
@@ -42,16 +42,18 @@ impl Debugger
         {
             if self.mempage_offset == 0x0000 { self.mempage_offset = 0x10000; }
             self.mempage_offset -= 0x400;
-        }        
+        }
+
+        self.draw_border();
         
         // dump memory page to screen
-        let mut start = 0x0000 + self.mempage_offset;
+        let mut start = 0x0000 + self.mempage_offset as u16;
 
         let mut title = Vec::new();
-        let _ = write!(&mut title, "Page ${:04x}-${:04x}", start, start + 0x3FF);
+        let _ = write!(&mut title, "Memory page ${:04x}-${:04x}", start, start + 0x3FF);
         self.font.draw_text(&mut self.window_buffer, 0, 0, &String::from_utf8(title).unwrap().to_owned()[..], 0x0A);
         let mut hex_offset_x = 0;
-        let mut hex_offset_y = 27;
+
         for y in 0..25
         {
             for x in 0..40
@@ -59,7 +61,7 @@ impl Debugger
                 let byte = memory.borrow_mut().get_ram_bank(c64::memory::MemType::RAM).read(start);
                 self.font.draw_char(&mut self.window_buffer, 8*x as usize, 8 + 8*y as usize, byte, 0x05);
 
-                self.draw_hex(hex_offset_x + x as usize, hex_offset_y + y as usize, byte);
+                self.draw_hex(hex_offset_x + x as usize, 27 + y as usize, byte);
                 hex_offset_x += 1;
                 start += 1;
             }
@@ -67,7 +69,8 @@ impl Debugger
             hex_offset_x = 0;
         }
 
-        self.draw_registers(memory);
+        self.draw_gfx_mode(memory);
+        self.draw_data(memory);
         
         self.debug_window.update(&self.window_buffer);
     }
@@ -87,7 +90,7 @@ impl Debugger
         self.font.draw_text_rgb(&mut self.window_buffer, x_pos, y_pos, &String::from_utf8(hex_value).unwrap().to_owned()[..], base_color);        
     }
 
-    fn draw_registers(&mut self, memory: &mut c64::memory::MemShared)
+    fn draw_data(&mut self, memory: &mut c64::memory::MemShared)
     {
         let d018 = memory.borrow_mut().get_ram_bank(c64::memory::MemType::IO).read(0xD018);
         let dd00 = memory.borrow_mut().get_ram_bank(c64::memory::MemType::IO).read(0xDD00);
@@ -96,17 +99,46 @@ impl Debugger
         let mut char_txt = Vec::new();
         let mut bmp_txt = Vec::new();
         let mut bank_txt = Vec::new();
-        let _ = write!(&mut vmatrix_txt, "  Screen: ${:04X}", (((d018 >> 4) & 0xF) as u16) * 0x400);
-        let _ = write!(&mut char_txt, "    Char: ${:04X}", (((d018 >> 1) & 0x07) as u16) * 0x800);
-        let _ = write!(&mut bmp_txt, "  Bitmap: ${:04X}", (((d018 >> 3) & 0x01) as u16) * 0x2000);
-        let _ = write!(&mut bank_txt, "VIC bank: ${:04X}", dd00);
-        self.font.draw_text(&mut self.window_buffer, 41, 1, &String::from_utf8(vmatrix_txt).unwrap().to_owned()[..], 0x0A);
-        self.font.draw_text(&mut self.window_buffer, 41, 2, &String::from_utf8(char_txt).unwrap().to_owned()[..], 0x0A);
-        self.font.draw_text(&mut self.window_buffer, 41, 3, &String::from_utf8(bmp_txt).unwrap().to_owned()[..], 0x0A);
-        self.font.draw_text(&mut self.window_buffer, 41, 4, &String::from_utf8(bank_txt).unwrap().to_owned()[..], 0x0A);
+        let _ = write!(&mut vmatrix_txt, "${:04X}", (((d018 >> 4) & 0xF) as u16) * 0x400);
+        let _ = write!(&mut char_txt, "${:04X}", (((d018 >> 1) & 0x07) as u16) * 0x800);
+        let _ = write!(&mut bmp_txt, "${:04X}", (((d018 >> 3) & 0x01) as u16) * 0x2000);
+        let _ = write!(&mut bank_txt, "${:04X}", dd00);
+        self.font.draw_text(&mut self.window_buffer, 43, 3, "Screen: ", 0x0F);
+        self.font.draw_text(&mut self.window_buffer, 51, 3, &String::from_utf8(vmatrix_txt).unwrap().to_owned()[..], 0x0E);
+        self.font.draw_text(&mut self.window_buffer, 45, 4, "Char: ", 0x0F);
+        self.font.draw_text(&mut self.window_buffer, 51, 4, &String::from_utf8(char_txt).unwrap().to_owned()[..], 0x0E);
+        self.font.draw_text(&mut self.window_buffer, 43, 5, "Bitmap: ", 0x0F);
+        self.font.draw_text(&mut self.window_buffer, 51, 5, &String::from_utf8(bmp_txt).unwrap().to_owned()[..], 0x0E);
+        self.font.draw_text(&mut self.window_buffer, 41, 6, "VIC Bank: ", 0x0F);
+        self.font.draw_text(&mut self.window_buffer, 51, 6, &String::from_utf8(bank_txt).unwrap().to_owned()[..], 0x0E);
+    }
+
+    fn draw_gfx_mode(&mut self, memory: &mut c64::memory::MemShared)
+    {
+        self.font.draw_text(&mut self.window_buffer, 52, 1, "ECM", 0x0B);
+        self.font.draw_text(&mut self.window_buffer, 57, 1, "CHR", 0x0B);
+        self.font.draw_text(&mut self.window_buffer, 62, 1, "BMP", 0x0B);
+        self.font.draw_text(&mut self.window_buffer, 67, 1, "MCM", 0x0B);
+    }
+
+    fn draw_border(&mut self)
+    {
+        for x in 0..80
+        {
+            self.font.draw_char(&mut self.window_buffer, 8*x as usize, 0, 64, 0x0B);
+            self.font.draw_char(&mut self.window_buffer, 8*x as usize, 8*26, 64, 0x0B);
+        }
+        
+        for y in 1..26
+        {
+            self.font.draw_char(&mut self.window_buffer, 8*40, 8*y as usize, 66, 0x0B);
+        }
+        
+        self.font.draw_char(&mut self.window_buffer, 8*40, 0, 114, 0x0B);
+        self.font.draw_char(&mut self.window_buffer, 8*40, 8*26, 113, 0x0B);
     }
     
-    fn set_saturation(&self, color: &mut u32, change: f64)
+   /* fn set_saturation(&self, color: &mut u32, change: f64)
     {
         let Pr = 0.299;
         let Pg = 0.587;
@@ -124,5 +156,5 @@ impl Debugger
         *color = (r as u32) << 16;
         *color |= (g as u32) << 8;
         *color |= b as u32;
-    }
+    }*/
 }
