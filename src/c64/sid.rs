593cff28df56a5f02cb8439cd6fd66c555572209
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2016-02-18 23:04:44 +0100

    initial filter calculations

--- src/c64/sid.rs
+++ src/c64/sid.rs
@@ -5,6 +5,7 @@ use c64::cpu;
 use c64::sid_tables::*;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::f32;
 
 pub type SIDShared = Rc<RefCell<SID>>;
 
@@ -130,7 +131,25 @@ pub struct SID
 {
     mem_ref: Option<memory::MemShared>,
     last_sid_byte: u8,
+    volume: u8,
+    filter_type: FilterType,
+    filter_freq: u8,
+    filter_resonance: u8,
+
+    // IIR filter
+    iir_att: f32,
+    d1: f32,
+    d2: f32,
+    g1: f32,
+    g2: f32,
+    xn1: f32,
+    xn2: f32,
+    yn1: f32,
+    yn2: f32,
+    
     voices: Vec<SIDVoice>,
+    sample_buffer: [u8; NUM_SAMPLES],
+    sample_idx: usize,
     rng: u32,
 }
 
@@ -143,6 +162,21 @@ impl SID
             mem_ref: None,
             last_sid_byte: 0,
             voices: vec![SIDVoice::new(), SIDVoice::new(), SIDVoice::new()],
+            volume: 0,
+            filter_type: FilterType::None,
+            filter_freq: 0,
+            filter_resonance: 0,
+            iir_att: 1.0,
+            d1: 0.0,
+            d2: 0.0,
+            g1: 0.0,
+            g2: 0.0,
+            xn1: 0.0,
+            xn2: 0.0,
+            yn1: 0.0,
+            yn2: 0.0,
+            sample_buffer: [0; NUM_SAMPLES],
+            sample_idx: 0,
             rng: 1
         }));
 
@@ -174,16 +208,31 @@ impl SID
 
     pub fn reset(&mut self)
     {
-        // TODO
         self.last_sid_byte = 0;
 
         for i in 0..self.voices.len()
         {
             self.voices[i].reset();
         }
+
+        self.volume = 0;
+        self.filter_type = FilterType::None;
+        self.filter_freq = 0;
+        self.filter_resonance = 0;
+        self.xn1 = 0.0;
+        self.xn2 = 0.0;
+        self.yn1 = 0.0;
+        self.yn2 = 0.0;
+        self.sample_idx = 0;
+        self.calculate_filter();
+
+        for i in 0..NUM_SAMPLES
+        {
+            self.sample_buffer[i] = 0;
+        }
     }
 
-    fn lowpass_resonance(&self, f: f64) -> f64
+    fn lowpass_resonance(&self, f: f32) -> f32
     {
         let f2 = f * f;
         let f3 = f2 * f;
@@ -191,13 +240,88 @@ impl SID
         227.755 - f - 1.7653 * f - 0.0176385 * f2 + 0.00333484 * f3 - 9.05683E-6 * f4
     }
 
-    fn highpass_resonance(&self, f: f64) -> f64
+    fn highpass_resonance(&self, f: f32) -> f32
     {
         let f2 = f * f;
         let f3 = f2 * f;
         366.374 - 14.0052 * f + 0.603212 * f2 - 0.000880196 * f3
     }    
 
+    fn calculate_filter(&mut self)
+    {
+        let f = self.filter_freq as f32;
+        let mut resonance: f32 = 0.0;
+        let mut arg: f32 = 0.0;
+        
+        match self.filter_type
+        {
+            FilterType::None => {
+                self.d1 = 0.0;
+                self.d2 = 0.0;
+                self.g1 = 0.0;
+                self.g2 = 0.0;
+                self.iir_att = 1.0;
+                return;
+            },
+            FilterType::All => {
+                self.d1 = 0.0;
+                self.d2 = 0.0;
+                self.g1 = 0.0;
+                self.g2 = 0.0;
+                self.iir_att = 0.0;
+                return;
+            }
+            FilterType::Lowpass | FilterType::LowBandpass => {
+               resonance = self.lowpass_resonance(f);
+            },
+            _ => {
+                resonance = self.highpass_resonance(f);
+            }
+        }
+
+        arg = resonance / ((SAMPLE_FREQ >> 1) as f32);
+        if arg > 0.99 { arg = 0.99; }
+        if arg < 0.01 { arg = 0.01; }
+
+        self.g2 = 0.55 + 1.2 * arg * arg - 1.2 * arg + resonance * 0.0133333333;
+        self.g1 = -2.0 * self.g2.sqrt() * (f32::consts::PI * arg).cos();
+
+        match self.filter_type {
+            FilterType::LowBandpass | FilterType::HighBandpass => self.g2 += 0.1,
+            _ => ()
+        }
+
+        if self.g1.abs() >= (self.g2 + 1.0)
+        {
+            if self.g1 > 0.0 { self.g1 = self.g2 + 0.99;    }
+            else             { self.g1 = -(self.g2 + 0.99); }
+        }
+
+        match self.filter_type {
+            FilterType::LowBandpass | FilterType::Lowpass => {
+                self.d1 = 0.0;
+                self.d2 = 1.0;
+                self.iir_att = 0.25 * (1.0 + self.g1 + self.g2);
+            },
+            FilterType::HighBandpass | FilterType::Highpass => {
+                self.d1 = -2.0;
+                self.d2 = 1.0;
+                self.iir_att = 0.25 * (1.0 - self.g1 + self.g2);
+            },
+            FilterType::Bandpass => {
+                self.d1 = 0.0;
+                self.d2 = -1.0;
+                self.iir_att = 0.25 * (1.0 + self.g1 + self.g2) * (1.0 + (f32::consts::PI * arg).cos()) / (f32::consts::PI * arg).sin();
+            },
+            FilterType::Notch => {
+                self.d1 = -2.0 * (f32::consts::PI * arg).cos();
+                self.d2 = 1.0;
+                self.iir_att = 0.25 * (1.0 + self.g1 + self.g2) * (1.0 + (f32::consts::PI * arg).cos()) / (f32::consts::PI * arg).sin();
+            },
+            _ => ()
+        }
+    }
+    
     fn get_rand(&mut self) -> u8
     {
         self.rng = self.rng * 1103515245 + 12345;
@@ -246,6 +370,8 @@ impl SID
     
     pub fn update(&mut self)
     {
-        // TODO
+        let idx = self.sample_idx;
+        self.sample_buffer[idx] = self.volume;
+        self.sample_idx = (self.sample_idx + 1) % NUM_SAMPLES;
     }
 }
